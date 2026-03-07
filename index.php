deed2f7e7b50f25bc493a597241579b511d81e3e
alexalvesx <alex_alves.x@hotmail.com>
2026-03-06 22:54:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html LANG="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="dash.css">
+
+    </head>
+
+    <body>
+
+<div class ="dashboard-container">
+    <nav>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">relatórios</a></li>
+            <li><a href="#">Configurações</a></li>
+        </ul>
+        <div class="perfil-usuario">
+            <img src="https://ui-avatars.com/api/?name=Alex+Alves&background=008080&color=fff" alt="Avatar">
+
+            <span>Alex ALves</span>
+        </div>
+    </nav>
+
+
+
+    
+
+    <main>
+        <section class="container-card">
+
+        <article>
+            <h2>Módulo de Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+
+        <article>
+            <h2>Módulo de Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+
+        <article>
+            <h2>Módulo de Usuários</h2>
+            <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+            <a href="#" class="btn">Acessar</a>
+        </article>
+
+    </main>
+</div>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> - Desenvolvido na aula de web I</p>
+    </footer>
+
+</body>
+</html>
